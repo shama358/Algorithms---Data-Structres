d6bf5ac1f1ecdb9ece42893b3f346bea8dfa55ea
Shama Hegde <shama358@gmail.com>
2017-08-05 16:12:09 -0700

    adding a new problem

---
+++ BackTracking_ExhaustiveSearch/CombinationSumI.java
@@ -0,0 +1,56 @@
+/* Question
+Given a set of candidate numbers (C) (without duplicates) and a target number 
+(T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [2, 3, 6, 7] and target 7, 
+A solution set is: 
+[
+  [7],
+  [2, 2, 3]
+]
+*/
+
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+        comboSumRec(candidates, target, result, 0, 0,new ArrayList<Integer>());
+        return result;
+    }
+    private void comboSumRec(int[] candidates, int target, List<List<Integer>> 
+	result, int sum, int idx, ArrayList<Integer> interRes) {
+        if (sum == target) {
+		
+        /* since java is pass by value, the ref of the interRes will be passed. 
+		So you create a new Object with the original interRes elements and add 
+		that to result. */
+		
+            result.add(new ArrayList<Integer>(interRes));
+            return;
+        }
+        if (sum > target) {
+            return;
+        }
+        /* for loop : the first postion can be filled in (candiate.length - 1) 
+		ways and so on for other positions as well.*/
+        /*the for loop is starting from idx as the combinations require not be 
+		repeated. so you ensure that by startng from the current     
+        position and not go to the left of the element.*/
+        for (int i = idx; i < candidates.length; ++i) {
+            int currNum = candidates[i];
+            interRes.add(currNum);
+            sum += candidates[i];
+            //consider the element by just adding the current number.
+            comboSumRec(candidates, target, result, sum , i, interRes);
+            //remove the current number and check wiht the next number.
+            sum -= candidates[i];
+            interRes.remove(interRes.size() - 1);
+        }
+        return;        
+    }
+}
